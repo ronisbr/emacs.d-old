cf4d3821106c59a4c4ebc0df5c826691bd361257
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2013-11-14 15:11:08 -0200

    Improve cc-mode setup

--- setup-cc-mode.el
+++ setup-cc-mode.el
@@ -5,13 +5,15 @@
 
 ;; My coding style.
 (c-add-style "my-coding-style"
-    '((c-offsets-alist . ((innamespace . 0)))))
+    '((c-offsets-alist . ((innamespace . 0)
+                          (substatement-open . 0)))))
 
 (add-hook 'c-mode-common-hook 
     '(lambda ()
-        (setq c-default-sytle "linux"
-              c-basic-offset 4)
-        (local-set-key (kbd "<f9>") 'recompile)
-        (c-set-style "my-coding-style")))
+         (setq c-basic-offset 4
+               c-indent-level 4)
+         (c-set-offset 'substatement-open 0)
+         (local-set-key (kbd "<f9>") 'recompile)
+         (c-set-style "my-coding-style")))
 
 (provide 'setup-cc-mode)
