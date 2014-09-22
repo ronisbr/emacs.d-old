816f07ce879ef9046eda111935a8064e6322a685
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2014-09-22 16:46:02 -0300

    FIX: flycheck-clang-language-standard
    As stated in https://github.com/flycheck/flycheck/issues/333 by
    @lunaryorn
    
    Do not change flycheck-clang-language-standard globally, but change it
    via file or directory variables, or set it in a mode-hook, e.g.:
    
    (add-hook 'c++-mode-hook
      (lambda () (
         setq flycheck-clang-language-standard "c++11")))

--- setup-flycheck.el
+++ setup-flycheck.el
@@ -6,6 +6,8 @@
 (require 'flycheck)
 
 ;; Set C++11 standard.
-(setq flycheck-clang-language-standard "c++11")
+(add-hook 'c++-mode-hook 
+  (lambda () (
+     setq flycheck-clang-language-standard "c++11")))
 
 (provide 'setup-flycheck)
