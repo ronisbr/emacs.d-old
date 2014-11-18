5033c6e32b6b7905f671ce736ae7849c5087cf4b
Ronan Arraes Jardim Chagas <ronan.chagas@ronanchagas-ops.ete.inpe.br>
2014-11-18 14:16:31 -0200

    Add custom.el to save emacs custom-settings

---
+++ custom.el
@@ -0,0 +1,38 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex -synctex=1")
+ '(custom-safe-themes (quote ("645599a2aab022fd7677124515a3104a60ba64d2cafdd77a6e7703f8ae97250c" "c7471ce3bb42defac344b3ecfca74228731b5ab20f804fd1deb8e65dddeab26a" "7122873f9ac192e4f2cfafe1679fe6b3db658ac64593efe4bc10c52d7573c6c1" "943bff6eada8e1796f8192a7124c1129d6ff9fbd1a0aed7b57ad2bf14201fdd4" default)))
+ '(ecb-options-version "2.40")
+ '(erc-away-timestamp-format "[%H:%M:%S] ")
+ '(erc-fill-column 80)
+ '(erc-fill-function (quote erc-fill-static))
+ '(erc-fill-static-center 15)
+ '(erc-insert-timestamp-function (quote erc-insert-timestamp-left-and-right))
+ '(erc-modules (quote (autojoin button completion fill irccontrols keep-place list match menu move-to-prompt netsplit networks noncommands readonly ring smiley stamp spelling track)))
+ '(erc-nick "Ronis_BR")
+ '(erc-nick-uniquifier "_")
+ '(erc-port 8001)
+ '(erc-server "chat.freenode.org")
+ '(erc-timestamp-right-column 82)
+ '(mark-even-if-inactive t)
+ '(scroll-bar-mode (quote right))
+ '(send-mail-function nil)
+ '(transient-mark-mode 1))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bg:erc-color-face1 ((t (:background "black"))) t)
+ '(erc-default-face ((t (:foreground "goldenrod"))) t)
+ '(erc-input-face ((t (:foreground "gray"))) t)
+ '(erc-my-nick-face ((t (:foreground "white smoke" :weight bold))) t)
+ '(sr-active-path-face ((t (:foreground "yellow" :weight bold :height 120))) t)
+ '(sr-highlight-path-face ((t (:foreground "#ace6ac" :weight bold :height 120))) t)
+ '(sr-passive-path-face ((t (:foreground "lightgray" :weight bold :height 120))) t)
+ '(sr-tabs-active-face ((t (:inherit variable-pitch :background "dim gray" :weight bold :height 0.9))) t)
+ '(sr-tabs-inactive-face ((t (:inherit variable-pitch :height 0.9))) t))
+
