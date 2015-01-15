31b4239a2cf6f779b61b654a6f92466200f1af80
Ronan Arraes Jardim Chagas <ronan.chagas@ronanchagas-osn.ete.inpe.br>
2015-01-14 23:10:08 -0200

    Modify some configurations of anzu

--- local-lisp/setup-anzu.el
+++ local-lisp/setup-anzu.el
@@ -6,4 +6,10 @@
 (require 'anzu)
 (global-anzu-mode +1)
 
+(custom-set-variables
+  '(anzu-mode-lighter "")
+  '(anzu-deactivate-region t)
+  '(anzu-search-threshold 1000)
+  '(anzu-replace-to-string-separator " => "))
+
 (provide 'setup-anzu)
