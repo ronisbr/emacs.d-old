fdbeeefbac608f5421726bb5b3eab7694b321e1e
Ronan Arraes Jardim Chagas <ronan.chagas@ronanchagas-ops.ete.inpe.br>
2015-07-06 18:21:49 -0300

    Remove back-button from compile_packages.sh

--- compile_packages.sh
+++ compile_packages.sh
@@ -3,7 +3,7 @@
 cd site-lisp/
 
 # List of package to be compiled.
-PACKAGES=(auto-complete back-button company-mode ecb ESS git-modes helm magit matlab-emacs neotree)
+PACKAGES=(auto-complete company-mode ecb ESS git-modes helm magit matlab-emacs neotree)
 
 # Compile packages.
 for package in ${PACKAGES[*]}
